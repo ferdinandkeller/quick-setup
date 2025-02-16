a871b008bf309b98b7096350acf96ca5b08d12ae
Ferdinand Keller <ferdinand.keller@proton.me>
2025-02-16 18:27:00 +0100

    create repo & add first iteration

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# make sure we are in the home directory
+cd ~
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install cli tools
+brew install sevenzip git git-flow gh stow nushell tmux zellij yazi zoxide jesseduffield/lazygit/lazygit neovim gcc fzf ripgrep fd uv
+
+# install dotfiles
+git clone https://github.com/ferdinandkeller/dotfiles.git
+stow -t ~ dotfiles
+
+# install apps
+brew install --cask font-jetbrains-mono-nerd-font 1password 1password-cli ghostty visual-studio-code docker google-chrome
+
+# install oh-my-zsh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# lazy vim
+# git clone https://github.com/LazyVim/starter ~/.config/nvim
+# rm -rf ~/.config/nvim/.git
